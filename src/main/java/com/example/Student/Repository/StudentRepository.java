c9674e9fc97f54ed7f232124ad359770bff9b78b
e._jeong <108976557+Kej613@users.noreply.github.com>
2024-06-09 16:55:06 +0900

    front-end 추가

---
+++ src/main/java/com/example/Student/Repository/StudentRepository.java
@@ -0,0 +1,16 @@
+//package com.example.Student.Repository;
+//
+//import com.example.Student.domain.Student;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface StudentRepository{
+//    Student save(Student student); //회원을 저장하면 저장된 회원이 반환
+//    Optional<Student> findById(Long id);
+//    Optional<Student> findByName(String name);
+//    List<Student> findAll();
+//
+//}
+//
